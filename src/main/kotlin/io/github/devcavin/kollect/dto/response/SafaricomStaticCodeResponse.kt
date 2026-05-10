f160977868d01785574929b5e14c031265e639d1
Cavin <cavinlarry001@gmail.com>
2026-05-10 12:49:58 +0300

    Add safaricom-static-code, safaricom-stkpush endpoints

---
+++ src/main/kotlin/io/github/devcavin/kollect/dto/response/SafaricomStaticCodeResponse.kt
@@ -0,0 +1,8 @@
+package io.github.devcavin.kollect.dto.response
+
+data class SafaricomStaticCodeResponse(
+    val ResponseCode: String,
+    val RequestId: String,
+    val ResponseDescription: String,
+    val QRCode: String
+)
--- src/main/kotlin/io/github/devcavin/kollect/service/CreditBankService.kt
+++ src/main/kotlin/io/github/devcavin/kollect/service/CreditBankService.kt
@@ -1,8 +1,12 @@
 package io.github.devcavin.kollect.service
 
 import io.github.devcavin.kollect.dto.request.AccountStatementRequest
+import io.github.devcavin.kollect.dto.request.SafaricomStaticCodeRequest
+import io.github.devcavin.kollect.dto.request.SafaricomStkPushRequest
 import io.github.devcavin.kollect.dto.response.AccountStatementResponse
 import io.github.devcavin.kollect.dto.response.BalanceInquiryResponse
+import io.github.devcavin.kollect.dto.response.SafaricomStaticCodeResponse
+import io.github.devcavin.kollect.dto.response.SafaricomStkPushResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -29,17 +33,47 @@ class CreditBankService(
     }
 
     // account statement
-    fun accountStatement(request: AccountStatementRequest): AccountStatementResponse {
+    fun accountStatement(request: AccountStatementRequest): List<AccountStatementResponse> {
         return try {
             restClient.post()
                 .uri("/account-statement")
                 .body(request)
                 .retrieve()
-                .body<AccountStatementResponse>()
+                .body<List<AccountStatementResponse>>()
                 ?: error("Empty response from API")
 
         } catch (e: Exception) {
             logger.error("Account statement retrieval failed: ${e.message}")
+            throw e
+        }
+    }
+
+    // safaricom stk push
+    fun safaricomStkPush(request: SafaricomStkPushRequest): SafaricomStkPushResponse {
+        return try {
+            restClient.post()
+                .uri("/safaricom-stkpush")
+                .body(request)
+                .retrieve()
+                .body<SafaricomStkPushResponse>()
+            ?: error("Empty response from API")
+        } catch (e: Exception) {
+            logger.error("Safaricom STK push failed: ${e.message}")
+            throw e
+        }
+    }
+
+    // safaricom static code
+    fun safaricomStaticCode(request: SafaricomStaticCodeRequest): SafaricomStaticCodeResponse {
+        return try {
+            restClient.post()
+                .uri("/safaricom-static-code")
+                .body(request)
+                .retrieve()
+                .body<SafaricomStaticCodeResponse>()
+            ?: error("Empty response from API")
+        } catch (e: Exception) {
+            logger.error("Safaricom static code failed: ${e.message}")
             throw e
         }
     }
